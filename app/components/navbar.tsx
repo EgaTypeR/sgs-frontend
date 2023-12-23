b805a571c0094265e1bc9ad9c1bc539c0b13b909
egasetiawan <ega.rizky.setiawan@mail.ugm.ac.id>
2023-12-23 09:20:24 +0700

    init

---
+++ app/components/navbar.tsx
@@ -0,0 +1,17 @@
+import Link from 'next/link'
+
+const Page = () =>{
+  return(
+    <div>
+      <nav className="flex flex-row justify-center gap-4 my-4">
+        <Link href={'/Search'}>
+          Search
+        </Link>
+        <Link href={'/Add'}>
+          Add
+        </Link>
+      </nav>
+    </div>
+  )
+}
+export default Page;
